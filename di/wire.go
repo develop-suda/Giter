a3e600c0370fb498229ba7cc309ad314bd71cce8
develop-suda <developsuda@gmail.com>
2025-01-04 15:26:16 +0000

    Add dependency injection setup with Wire for controller initialization

---
+++ di/wire.go
@@ -0,0 +1,29 @@
+// // Copyright 2018 The Wire Authors
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     https://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
+
+// // The build tag makes sure the stub is not built in the final build.
+package di
+
+// import (
+// 	"giter/controllers"
+// 	"giter/repositories"
+// 	"giter/services"
+
+// 	"github.com/google/wire"
+// )
+
+// func InitializeRouter() controllers.IRequestController {
+// 	wire.Build(controllers.NewRequestController, services.NewRequestService, repositories.NewRequestRepository)
+// 	return &controllers.RequestController{}
+// }
